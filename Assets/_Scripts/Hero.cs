53864383789862f99f946cf5fced7b49488faf24
Robin <robimeht@umich.edu>
2016-09-15 15:55:23 -0400

    added shield and shield script

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -2,14 +2,34 @@
 using System.Collections;
 
 public class Hero : MonoBehaviour {
+	    static public Hero      S; // Singleton
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	    // These fields control the movement of the ship
+	    public float            speed = 30;
+	    public float            rollMult = -45;
+	    public float            pitchMult = 30;
+
+	    // Ship status information
+	    public float            shieldLevel = 1;
+
+	    public bool ____________________________;
+
+	    void Awake() {
+		        S = this;  // Set the Singleton
+		    }
+
+	    void Update () {
+		        // Pull in information from the Input class
+		        float xAxis = Input.GetAxis("Horizontal");                          // 1
+		        float yAxis = Input.GetAxis("Vertical");                            // 1
+
+		        // Change transform.position based on the axes
+		        Vector3 pos = transform.position;
+		        pos.x += xAxis * speed * Time.deltaTime;
+		        pos.y += yAxis * speed * Time.deltaTime;
+		        transform.position = pos;
+
+		        // Rotate the ship to make it feel more dynamic                     // 2
+		        transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult,0);
+		    }
 }
