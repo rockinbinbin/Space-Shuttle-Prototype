220a7e56e6f4906c85912ad3de00b027665c3b47
Robin <robimeht@umich.edu>
2016-09-16 22:46:45 -0400

    no errors

--- Assets/_Scripts/Weapon.cs
+++ Assets/_Scripts/Weapon.cs
@@ -45,8 +45,11 @@ public class Weapon : MonoBehaviour {
 	    public GameObject         collar;
 	    public float              lastShot; // Time last shot was fired
 
-	    void Start() {
-		        collar = transform.Find("Collar").gameObject;
+		void Awake() {
+			collar = transform.Find("Collar").gameObject;
+		}
+		
+	    void Start() { 
 		        // Call SetType() properly for the default _type
 		        SetType( _type );
 
